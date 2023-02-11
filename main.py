997b1c171a903477f7934be5d50fa2608102025e
Richard You <richardyou94@gmail.com>
2023-02-10 16:56:22 -0800

    done with basic function of rock paper and scissors

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,4 +26,45 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+
+match user_choice:
+    case "0":
+        user_choice = "rock"
+        print(rock)
+        print("You choose: ")
+    case "1":
+        user_choice = "paper"
+        print(paper)
+        print("You choose: ")
+    case "2":
+        user_choice = "scissors"
+        print(scissors)
+        print('You choose: ')
+
+imageChoices = [rock, paper, scissors]
+listOfChoices = ["rock", "paper", "scissors"]
+randomNumber = random.randint(0, 2)
+computer_choice = listOfChoices[randomNumber]
+print(imageChoices[randomNumber])
+print("Computer choose: ")
+
+
+
+if user_choice == computer_choice:
+    print("Draw!")
+elif user_choice == "rock" and computer_choice == "paper":
+    print('You lose!')
+elif user_choice == "rock" and computer_choice == "scissors":
+    print('You win!')
+elif user_choice == "paper" and computer_choice == "rock":
+    print('You win!')
+elif user_choice == "paper" and computer_choice == "scissors":
+    print('You lose!')
+elif user_choice == "rock" and computer_choice == "paper":
+    print("You lose!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print('You win!')
+
 
